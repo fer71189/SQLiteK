cd9d03a9264849f11215b6d4ca88d11ce17e405b
FernandoPR89 <109626048+FernandoPR89@users.noreply.github.com>
2024-08-05 23:10:04 -0600

    aplicacion
    version 1.0

---
+++ app/src/main/java/com/nextdev/notessqlitek/Note.kt
@@ -0,0 +1,7 @@
+package com.nextdev.notessqlitek
+
+data class Note(
+    val id: Int,
+    val title: String,
+    val content: String
+)
---
+++ app/src/main/java/com/nextdev/notessqlitek/UpdateActivity.kt
@@ -0,0 +1,46 @@
+package com.nextdev.notessqlitek
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.nextdev.notessqlitek.databinding.ActivityAddNoteBinding
+import com.nextdev.notessqlitek.databinding.ActivityUpdateBinding
+
+class UpdateActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUpdateBinding
+    private lateinit var db: NotesDatabaseHelper
+    private var noteId: Int = -1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        db = NotesDatabaseHelper(this)
+        noteId = intent.getIntExtra("note_id",-1)
+        if (noteId==-1){
+            finish()
+            return
+        }
+        val note = db.getNoteById(noteId)
+        binding.updateTitleEditText.setText(note.title)
+        binding.updateContentEditText.setText(note.content)
+
+        binding.updateSaveButton.setOnClickListener{
+            val newTitle = binding.updateTitleEditText.text.toString()
+            val newContent = binding.updateContentEditText.text.toString()
+            val updateNote = Note(noteId, newTitle, newContent)
+            db.updateNote(updateNote)
+            finish()
+            Toast.makeText(this, "Changes Saveed", Toast.LENGTH_LONG).show()
+        }
+    }
+}
